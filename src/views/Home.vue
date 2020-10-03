5b3115b02ccf95f7007f33e12e7b118a8f1bf410
Aurel Zefi <aurelzefi1994@gmail.com>
2020-10-03 22:06:10 +0200

    summary

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:font-light text-gray-700">
     <p class="mt-10">
-      Hi! I'm Aurel Zefi, a web developer from Tirana, Albania. I work mainly with Laravel and Vue, and recently also loving
+      Hi! I'm Aurel Zefi, a full-stack developer from Tirana, Albania. I work mainly with Laravel and Vue, and recently also loving
       <a href="https://tailwindcss.com" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">TailwindCSS</a>.
-      I like to create quality and maintainable products by writing clean and well designed code.
     </p>
 
     <p class="mt-6">
-      I am not currently available for new work, but feel free to
-      <a href="mailto:aurelzefi1994@gmail.com" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900 whitespace-no-wrap">contact me</a>
-      for any other inquiries.
+      I like to create quality and maintainable products by writing clean and well designed code.
     </p>
   </div>
 </template>
