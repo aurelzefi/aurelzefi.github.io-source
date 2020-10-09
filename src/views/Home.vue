2488f6a87115dbe81d9304d4d9796931c9abfa3e
aurelzefi <aurel.zefi@eurostep.it>
2020-10-09 09:10:07 +0200

    text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,8 @@
     </p>
 
     <p class="mt-6">
+      I am experienced on building web applications, and recently also desktop apps with
+      <a href="https://www.electronjs.org" class="border-b border-gray-900 pb-px md:pb-1 text-gray-900">Electron</a>.
       I like to create quality and maintainable products by writing clean and well designed code.
     </p>
   </div>
